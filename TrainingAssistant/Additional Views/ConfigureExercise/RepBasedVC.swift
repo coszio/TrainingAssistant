14710309dc99ccb75f2fdad6895fa5f467980dbd
kcirym10 <kcirym10@me.com>
2020-10-26 16:41:06 -0400

    Included config views

---
+++ TrainingAssistant/Additional Views/ConfigureExercise/RepBasedVC.swift
@@ -0,0 +1,35 @@
+//
+//  RepBasedVC.swift
+//  TrainingAssistant
+//
+//  Created by user180996 on 10/26/20.
+//
+
+import UIKit
+
+class RepBasedVC: UIViewController {
+
+    @IBOutlet weak var sets: UITextField!
+    @IBOutlet weak var reps: UITextField!
+    @IBOutlet weak var restTime: UITextField!
+    @IBOutlet weak var finalRestTime: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

48afcd7d4a57d0b60861d312f2ad5c4c6f30504e
Luis Cossio <luis.cossio@outlook.com>
2020-11-02 19:51:40 -0500

    Ya funciona Configurar Ejercicio y se muestra en la seleccion de la rutina

---
+++ TrainingAssistant/Additional Views/Cells/ConfiguredExerciseTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ConfiguredExerciseTableViewCell.swift
+//  TrainingAssistant
+//
+//  Created by user181023 on 11/1/20.
+//
+
+import UIKit
+
+class ConfiguredExerciseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
